4e8f6293baa3a925923d090851a365ff95a264a6
SARTHAK\sarth <sarthakkalbande555@gmail.com>
2024-05-27 07:57:43 +0530

    Spring boot demo- aading gitignore,app properties, pom and app file

---
+++ src/main/java/com/vehicle/vehicle_information/VehicleInformationApplication.java
@@ -0,0 +1,13 @@
+package com.vehicle.vehicle_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInformationApplication.class, args);
+	}
+
+}
